966f531dfcc1b6fc6ac49a9f225e6ef1af99d055
Michael Odumosu <michaelodumosu57@gmail.com>
2019-02-24 12:35:28 -0500

    placing files where they belong

--- EVENT/DragEvent.js
+++ EVENT/DragEvent.js
@@ -2,5 +2,5 @@ var event = new DragEvent("abc");
 window.addEventListener("abc",function(e){
 console.log(e)
 console.log(e.dataTransfer)
-window.dispatchEvent(event)
 })
+window.dispatchEvent(event)
