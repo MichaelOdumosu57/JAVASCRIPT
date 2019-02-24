e55a1359b17352972d191ab09def4c70bf1eb6b7
Michael Odumosu <michaelodumosu57@gmail.com>
2019-02-24 12:32:48 -0500

    a

--- BeforeUnloadEvent.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("beforeunload", function (e) {
-  var confirmationMessage = "\o/";
-
-  (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
-  return confirmationMessage;                                //Webkit, Safari, Chrome etc.
-});
