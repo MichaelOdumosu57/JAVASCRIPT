7aadec6ed8c6bd90f4ac90efe3e4d80bf705721f
Michael Odumosu <michaelodumosu57@gmail.com>
2019-02-24 12:36:33 -0500

    snippet expansion

--- SNIPPETS_AND_TEMPLATES/pretty_print.js
+++ SNIPPETS_AND_TEMPLATES/pretty_print.js
@@ -1,9 +1,10 @@
 
+
 var pretty_print = {
-        seperator        : 5,
+        seperator        : 3,
         s_key            : "-",
         notes_length_max : -Infinity || -9999999999999999999,
-        notes_collection : "",
+        notes_collection : "{",
         a                :   {
                      '.altKey'         : 'true if keydown during event'
                     ,'.button'         : 'button number pressed during event'
@@ -18,9 +19,29 @@ var pretty_print = {
                     ,'.screenX'        : 'coord related screen dev coords (check this)'
                     ,'.screenY'        : 'coord related screen dev coords (check this)'
                     ,'.shiftKey'       : 'true if keydown during event'
+                    ,'.getModifierState ' : 'needs one KeyboardEvent.key to work true if pressed or locked'
                 },
-        b                :Object.entries(   this.a   ),
-        pretty_do        :function(){
+        b                :[] ,
+        c                :function(){pretty_print.b = Object.entries(   pretty_print.a   )},
+        pretty_do        :function(   dev_obj   ){
+
+                                if(   dev_obj != undefined   ){
+                                    
+                                    
+                                    if(   dev_obj.a != undefined   ){
+    
+    
+                                        pretty_print.a = dev_obj.a
+                                        
+                                    }
+                                
+                                
+                                }
+                                
+                                
+                                this.c()
+
+
                                 for(   var i= 0; i != this.b.length; i++   ){
                                 
                                 
@@ -34,7 +55,7 @@ var pretty_print = {
                                     this.notes_collection += "\n" + this.b[i][0] + Array(   this.notes_length_max  - this.b[i][0].length + this.seperator ).join(" ") + this.s_key
                                     
                                     
-                                     	if(   typeof(   b[i][1]   ) == 'object'   ){
+                                     	if(   typeof(   this.b[i][1]   ) == 'object'   ){
                                 
                                 
                                      		this.notes_collection += JSON.stringify(   this.b[i][1],null,2   )
@@ -53,10 +74,12 @@ var pretty_print = {
                                      	
                                      	
                                 }
-                                return this.notes_collection
+                                return this.notes_collection + "\n" + "}"
                             }
     
     } // will make this plugin OOP
+    pretty_print.pretty_do()
+    
 var seperator = 5  // spaces needed
 var s_key = "-"   //key used to make notes
 var notes_length_max = -Infinity || -9999999999999999999
@@ -108,3 +131,46 @@ for(   var i= 0; i != b.length; i++   ){
      	
      	
 }
+
+var pointer_props = {a:{
+            '.pointerId':'a unique ID for the pointer',
+            '.width':  'width in CSS pixels, of pointer contact geometry',
+            '.height':  'height in CSS pixels, of pointer contact geometry',
+            '.pressure': 'what pressure the machine can detect 0 for min it can detect 1 for max it can detect',
+            '.tangentialPressure':'cylinder stress from -1 to 1',
+            '.tiltX':'plane angle (degress from -90 to 90) btwn Y-Z plane and plane with transducer(stylus) and Y axis',
+            '.tiltY':'plane angle (degress from -90 to 90) btwn X-Z plane and plane with transducer(stylus) and X axis',
+            '.twist':'clockwise rotation of transducer (degrees 0 -359)',
+            '.pointerType': 'mouse, pen touch ...',
+            '.isPrimary':'is this the primary pointerType'
+    
+            }
+    }
+var progress_props = {a:{
+                            '.lengthComputable':'boolean for if the progress is measureable',
+                            '.loaded':' an unsigned long long of work done by child process. for HTTP downloads it does not consider headers and other metadata',
+                            '.total':'unsigned long long for the work child process has done'
+                        }
+    
+    }
+var keys = {        b: 'boolean if held during event',
+                    c:'Key'}
+var touch_props = { a:{
+                        '.altKey':'boolean if held during event',
+                        '.changedTouches':'a TouchList of all Touch objects at single contact points and the changes btwn them',
+                        '.ctrlKey':'boolean if held during event',
+                        '.metaKey':keys.b,
+                        '.shiftKey': keys.c,
+                        '.targetTouches':'all Touch object currently in contact with the surface and were started on the same element',
+                        '.touches':'TouchList of all Touch obj representing all current surface contact points',
+                        
+                        
+                      },
+                  }
+var UI_props = { a:{
+                        '.detail':'long for event details',
+                        
+                    }
+                }
+    
+                  
--- SNIPPETS_AND_TEMPLATES/ultraObject.js
+++ SNIPPETS_AND_TEMPLATES/ultraObject.js
@@ -4,13 +4,18 @@
 // for meaningful use, every property purpose is easily availble to each other
 var ultraObject = {
     eventName:"",           //wants a DOMString event name
+    event_obj:undefined,              //wants an Event
     DOM_child:{0:undefined}, // wants a DOM element
-    addEventListener:function(){ ultraObject.DOM_child[0].addEventListener(ultraObject.eventName,ultraObject.passing_args)},
-    passing_args: function(){console.log(arguments)} // use this to see parameters from functions that have something to offer
+    addEventListener:function(   dev_obj   ){ ultraObject.DOM_child[0].addEventListener(   ultraObject.eventName,dev_obj.fn !== undefined ? dev_obj.fn : ultraObject.passing_args   )},
+    passing_args: function(   dev_obj   ){console.log(arguments)}, // use this to see parameters from functions that have something to offer
+    dispatchEvent: function(   dev_obj   ){ultraObject.DOM_child[0].dispatchEvent(   ultraObject.event_obj   )}    // sync fires events
         
     
     }
-    
-ultraObject.eventName = 'click'
-ultraObject.DOM_child[0] = document
-ultraObject.addEventListener()
+
+// var a = new UIEvent('b')
+ ultraObject.eventName = 'onload'
+ ultraObject.event_obj = a
+ ultraObject.DOM_child[0] = document.querySelector('canvas')
+ ultraObject.addEventListener()
+ ultraObject.dispatchEvent()
